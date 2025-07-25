800f3456611a33a1801e7d12f8340852199e8bc0
jeanheberth <jeanheberth19@gmail.com>
2025-07-25 12:08:37 -0300

    Criado lwtFilter

--- src/main/java/com/br/financaspessoais/config/SecurityConfig.java
+++ src/main/java/com/br/financaspessoais/config/SecurityConfig.java
@@ -5,20 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll() // 🔓 Login permitido sem autenticação
+                        .requestMatchers("/api/auth/**").permitAll() // 🔓 Login permitido sem autenticação
                         .anyRequest().authenticated()
-                );
-
-        return http.build();
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
--- src/main/java/com/br/financaspessoais/controller/LancamentoController.java
+++ src/main/java/com/br/financaspessoais/controller/LancamentoController.java
@@ -2,8 +2,10 @@ package com.br.financaspessoais.controller;
 
 import com.br.financaspessoais.dto.in.LancamentoRequestDTO;
 import com.br.financaspessoais.dto.out.LancamentoResponseDTO;
+import com.br.financaspessoais.model.Lancamento;
 import com.br.financaspessoais.service.LancamentoService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/lancamentos")
+@Slf4j
 public class LancamentoController {
 
     @Autowired
@@ -22,7 +25,10 @@ public class LancamentoController {
 
     @PostMapping
     public ResponseEntity<LancamentoResponseDTO> criarLancamento(@RequestBody @Valid LancamentoRequestDTO lancamentoRequestDto) {
-        return ResponseEntity.ok(lancamentoService.salvar(lancamentoRequestDto));
+        log.info("📩 Recebido lançamento: {}", lancamentoRequestDto);
+        LancamentoResponseDTO salvo = lancamentoService.salvar(lancamentoRequestDto);
+        log.info("✅ Lançamento salvo: {}", salvo);
+        return ResponseEntity.ok(salvo);
     }
 
     @GetMapping
