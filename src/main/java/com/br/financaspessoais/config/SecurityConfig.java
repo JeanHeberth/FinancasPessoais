5598066df30bea6fd0b1530bef503ce6ae2deaca
jeanheberth <jeanheberth19@gmail.com>
2025-07-25 11:10:44 -0300

    Criado jwt

---
+++ src/main/java/com/br/financaspessoais/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.br.financaspessoais.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll() // 🔓 Login permitido sem autenticação
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
