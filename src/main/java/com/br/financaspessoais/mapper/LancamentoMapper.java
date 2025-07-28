6ebdb5f41f8d29113dcd41347fbb153ffd74b524
jeanheberth <jeanheberth19@gmail.com>
2025-07-28 13:02:39 -0300

    Corrigindo os mapStruct.

--- src/main/java/com/br/financaspessoais/mapper/LancamentoMapper.java
+++ src/main/java/com/br/financaspessoais/mapper/LancamentoMapper.java
@@ -4,10 +4,13 @@ import com.br.financaspessoais.dto.in.LancamentoRequestDTO;
 import com.br.financaspessoais.dto.out.LancamentoResponseDTO;
 import com.br.financaspessoais.model.Lancamento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper( componentModel = "spring" )
 public interface LancamentoMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "usuario", ignore = true)
     Lancamento toEntity(LancamentoRequestDTO lancamentoRequestDTO);
     LancamentoResponseDTO toResponseDTO(Lancamento lancamento);
 }
